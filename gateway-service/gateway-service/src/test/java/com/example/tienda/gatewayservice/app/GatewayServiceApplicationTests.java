1df297da53555da9d0c973cb4b704b86394688e3
Anderson Bengolea <anderson.bengolea.contractor@bbva.com>
2022-11-18 20:12:27 -0500

    first commit

---
+++ gateway-service/gateway-service/src/test/java/com/example/tienda/gatewayservice/app/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tienda.gatewayservice.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
